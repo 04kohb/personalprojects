d3276cca8e93d3852cc624b34fe8ce4ffe34abe6
Boon Yeow Koh <yeow.koh@gmail.com>
2021-06-13 00:08:58 +0800

    adding new files

---
+++ src/robots.js
@@ -0,0 +1,26 @@
+export const robots =[
+{
+	id: 0,
+	name: 'Lisa Graham',
+	username: 'Lgraham',
+	email: 'lgraham@gmail.com'
+},
+{
+	id: 1,
+	name: 'Mika Graham',
+	username: 'Mgraham',
+	email: 'Mgraham@gmail.com'
+},
+{
+	id: 2,
+	name: 'Kiki Graham',
+	username: 'Mgraham',
+	email: 'rgraham@gmail.com'
+},
+{
+	id: 3,
+	name: 'Rikita Graham',
+	username: 'Rgraham',
+	email: 'rgraham@gmail.com'
+}
+];
